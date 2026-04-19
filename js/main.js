f802e81b74d61ee8dfd46d8328ae7de7b95fa5d6
VICMAR SERVICES <39407749+fcalvopi@users.noreply.github.com>
2026-04-19 12:40:55 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const form = document.getElementById("formulario");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const nombre = document.getElementById("nombre").value;
+  const email = document.getElementById("email").value;
+  const mensaje = document.getElementById("mensaje").value;
+
+  const data = { nombre, email, mensaje };
+
+  try {
+    const res = await fetch("https://vicmar-pro-3.onrender.com/contacto", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+
+    if (res.ok) {
+
+      // 📲 MENSAJE AUTOMÁTICO WHATSAPP
+      const texto = `Hola, soy ${nombre}. Necesito información sobre: ${mensaje}`;
+      const url = `https://wa.me/593969054410?text=${encodeURIComponent(texto)}`;
+
+      window.open(url, "_blank");
+
+      form.reset();
+
+    } else {
+      alert("Error al enviar formulario");
+    }
+
+  } catch (error) {
+    alert("Error de conexión");
+  }
+});
